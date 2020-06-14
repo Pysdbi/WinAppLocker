8bf1ee8ba4f00757b2f61a9810c88955f7e98bb9
pysdbi <pysdbi@yandex.ru>
2020-06-14 20:08:12 +0300

    v1.0

---
+++ WinAppLocker/MainWindow.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace WinAppLocker
+{
+	public partial class MainWindow : Window
+	{
+        [DllImport("user32", EntryPoint = "SetWindowsHookExA", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
+        public static extern int SetWindowsHookEx(int idHook, LowLevelKeyboardProcDelegate lpfn, int hMod, int dwThreadId);
+
+
+        [DllImport("user32", EntryPoint = "UnhookWindowsHookEx", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
+        public static extern int UnhookWindowsHookEx(int hHook);
+
+
+        public delegate int LowLevelKeyboardProcDelegate(int nCode, int wParam, ref KBDLLHOOKSTRUCT lParam);
+
+
+        [DllImport("user32", EntryPoint = "CallNextHookEx", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
+        public static extern int CallNextHookEx(int hHook, int nCode, int wParam, ref KBDLLHOOKSTRUCT lParam);
+
+
+        public const int WH_KEYBOARD_LL = 13;
+
+
+        [DllImport("user32.dll")]
+        private static extern int FindWindow(string className, string windowText);
+
+        [DllImport("user32.dll")]
+        private static extern int ShowWindow(int hwnd, int command);
+
+        private const int SW_HIDE = 0;
+        private const int SW_SHOW = 1;
+
+        public struct KBDLLHOOKSTRUCT
+        {
+            public int vkCode;
+            public int scanCode;
+            public int flags;
+            public int time;
+            public int dwExtraInfo;
+        }
+
+        public static int intLLKey;
+
+
+        public MainWindow()
+		{
+			InitializeComponent();
+
+			this.WindowState = WindowState.Maximized;
+
+            intLLKey = SetWindowsHookEx(WH_KEYBOARD_LL,
+                                        LowLevelKeyboardProc,
+                                        Marshal.GetHINSTANCE(System.Reflection.Assembly.GetExecutingAssembly().GetModules()[0]).ToInt32(),
+                                        0);
+            KillCtrlAltDelete();
+        }
+
+
+        public int LowLevelKeyboardProc(int nCode, int wParam, ref KBDLLHOOKSTRUCT lParam)
+        {
+            bool blnEat = false;
+
+            switch (wParam)
+            {
+                case 256:
+                case 257:
+                case 260:
+                case 261:
+                    //Alt+Tab, Alt+Esc, Ctrl+Esc, Windows Key,
+                    blnEat = ((lParam.vkCode == 9) && (lParam.flags == 32)) |
+                            ((lParam.vkCode == 27) && (lParam.flags == 32)) |
+                            ((lParam.vkCode == 27) && (lParam.flags == 0)) |
+                            ((lParam.vkCode == 91) && (lParam.flags == 1)) |
+                            ((lParam.vkCode == 92) && (lParam.flags == 1)) |
+                            ((lParam.vkCode == 73) && (lParam.flags == 0));
+                    break;
+            }
+
+            if (blnEat)
+            {
+                return 1;
+            }
+            else
+            {
+                return CallNextHookEx(0, nCode, wParam, ref lParam);
+            }
+        }
+
+        public void KillCtrlAltDelete()
+        {
+            RegistryKey regkey;
+            string keyValueInt = "1";
+            string subKey = @"Software\Microsoft\Windows\CurrentVersion\Policies\System";
+
+            try
+            {
+                regkey = Registry.CurrentUser.CreateSubKey(subKey);
+                regkey.SetValue("DisableTaskMgr", keyValueInt);
+                regkey.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public void KillStartMenu()
+        {
+            int hwnd = FindWindow("Shell_TrayWnd", "");
+            ShowWindow(hwnd, SW_HIDE);
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Escape)
+			{
+				this.Close();
+			}
+		}
+
+		private void SignIn_Click(object sender, RoutedEventArgs e)
+		{
+            int hwnd = FindWindow("Shell_TrayWnd", "");
+            ShowWindow(hwnd, SW_SHOW);
+
+            try
+            {
+                string subKey = @"Software\Microsoft\Windows\CurrentVersion\Policies\System";
+                RegistryKey rk = Registry.CurrentUser;
+                RegistryKey sk1 = rk.OpenSubKey(subKey);
+                if (sk1 != null)
+                    rk.DeleteSubKeyTree(subKey);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //e.Cancel = true;
+            //base.OnClosing(e);
+            
+        }
+    }
+}
